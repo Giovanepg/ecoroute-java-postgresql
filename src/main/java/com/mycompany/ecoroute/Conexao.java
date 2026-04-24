12c4c7a24ea18896745fd7607ac4e4b60e8ebc4e
Giovane Ferreira <ferreirapaesribeirog@gmail.com>
2026-04-24 11:16:00 -0300

    Clean up Conexao.java by removing comments
    Removed license comments and author information.

--- src/main/java/com/mycompany/ecoroute/Conexao.java
+++ src/main/java/com/mycompany/ecoroute/Conexao.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.ecoroute;
 
-/**
- *
- * @author blacklegen
- */
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
